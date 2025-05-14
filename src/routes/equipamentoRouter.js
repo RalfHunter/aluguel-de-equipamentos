0e246b39a0b8b96fd61b1f08232fd99322db3e46
Taline Rodrigues <talinefranca32@gmail.com>
2025-05-14 09:33:36 -0400

    finalizando implementação das rotas de equipamento

--- src/routes/equipamentoRouter.js
+++ src/routes/equipamentoRouter.js
@@ -1,15 +1,24 @@
 import express from "express";
 import { asyncWrapper } from '../utils/helpers/index.js';
+import EquipamentoController from "../controllers/EquipamentoController.js";
 
 const router = express.Router();
-
-const EquipamentoController = new EquipamentoController(); 
+const equipamentoController = new EquipamentoController();
 
 router
-    .get("/equipamentos", asyncWrapper(EquipamentoController.listar.bind(EquipamentoController)))
-    .get("/equipamentos/:id", asyncWrapper(EquipamentoController.listar.bind(EquipamentoController)))
-    .post("/equipamentos",  asyncWrapper(EquipamentoController.criar.bind(EquipamentoController)))
-    .patch("/equipamentos/:id", asyncWrapper(EquipamentoController.atualizar.bind(EquipamentoController)))
-    .delete("/equipamentos/:id", asyncWrapper(EquipamentoController.deletar.bind(EquipamentoController)))
+    //lista todos os equipamentos cadastrados
+    .get("/equipamentos", /*AuthMiddleware, authPermission,*/ asyncWrapper(equipamentoController.listar.bind(equipamentoController)))
+
+    //lista um equipamento especifico
+    .get("/equipamentos/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(equipamentoController.obterPorId.bind(equipamentoController)))
+
+    //cadastrar um equipamento
+    .post("/equipamentos", /*AuthMiddleware, authPermission,*/ asyncWrapper(equipamentoController.criar.bind(equipamentoController)))
+
+    //atualizar dados do equipamento (que esteja ativo)
+    .patch("/equipamentos/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(equipamentoController.atualizar.bind(equipamentoController)))
+    
+    //INATIVA o equipamento
+    .delete("/equipamentos/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(equipamentoController.deletar.bind(equipamentoController)));
 
 export default router;
