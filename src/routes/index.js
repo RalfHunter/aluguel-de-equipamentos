543d88e20aa8e318a7e0f8218bd67170f42c735f
Taline Rodrigues <talinefranca32@gmail.com>
2025-05-17 15:03:54 -0400

    corrigindo erro de importação reserva routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import express from "express";
 import logRoutes from "../middlewares/LogRoutesMiddleware.js";
 import dotenv from "dotenv";
 
-import reserva from "./reservaRouter.js"
+import reserva from "./reservaRoutes.js"
 
 dotenv.config();
 
