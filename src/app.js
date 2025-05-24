be151ed102592104042965af29fa7c1fb488ddd0
Taline Rodrigues <talinefranca32@gmail.com>
2025-05-24 00:06:17 -0400

    arrumando importação DbConnect - conflito

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import routes from "./routes/index.js";
 import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
-import DbConnect from './config/DbConnect.js';
+import DbConnect from './config/Dbconnect.js';
 import errorHandler from './utils/helpers/errorHandler.js';
 import logger from './utils/logger.js';
 import CommonResponse from './utils/helpers/CommonResponse.js';
