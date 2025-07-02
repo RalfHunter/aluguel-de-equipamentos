e4e63282ae5889fd9cf429a47f2f00fef5923fc6
Taline Rodrigues <talinefranca32@gmail.com>
2025-07-01 20:20:37 -0400

    removendo arquivo desnecessários

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,7 @@ export default  {
     // testMatch:['**/UsuarioController.test.js']
     // testMatch:['**/AuthMiddleware.test.js']]
     // testMatch:['**/usuarioRouter.test.js']
-    testMatch:['**/authRouter.test.js']
+    testMatch:['**/equipamentoRouter.test.js']
 
 
 };
